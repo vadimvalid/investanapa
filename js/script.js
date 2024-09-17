e2ea85645a60623f19c1ec3f2239a503c89af3c7
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-09-17 23:25:36 +0300

    Add slider

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Splide("#image-carousel", {
+  type: "loop",
+  interval: 4000,
+  autoplay: true,
+  speed: 2000,
+  perMove: 1,
+  rewind: true,
+  easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+  arrows: false,
+}).mount();
